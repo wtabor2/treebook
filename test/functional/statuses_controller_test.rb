28ee1ab03ed0f253cad8d3209f5f4fbd95547f74
Ren Tabor <tabor.warren@gmail.com>
2013-05-03 16:47:36 -0400

    Add status validation snad tests

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -22,8 +22,15 @@ class StatusesControllerTest < ActionController::TestCase
     get :new
     assert_response :success
   end
+  
+  test "should be logged in to post statues" do
+    post :create, status: {content: "Hello"}
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
 
-  test "should create status" do
+  test "should create status when logged in" do
+    sign_in users(:ren)
     assert_difference('Status.count') do
       post :create, status: { content: @status.content}
     end
